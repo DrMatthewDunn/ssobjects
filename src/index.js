7ecc8a75aaadc3fa708290434a72ab1f40890eeb
Matthew Dunn <matthew@sayitvisually.com>
2023-12-07 15:34:43 -0800

    v1 sample code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { ThrowableRouter, missing, withParams } from 'itty-router-extras'
+import { withDurables } from 'itty-durable'
 
+// export the durable class, per spec
+export { Counter } from './Counter'
+
+const router = ThrowableRouter({ base: '/counter' })
+
+router
+  // add upstream middleware, allowing Durable access off the request
+  .all('*', withDurables())
+
+  // get the durable itself... returns json response, so no need to wrap
+  .get('/', ({ Counter }) => Counter.get('test').toJSON())
+
+  // By using { autoReturn: true } in createDurable(), this method returns the contents
+  .get('/increment', ({ Counter }) => Counter.get('test').increment())
+
+  // you can pass any serializable params to a method... (e.g. /counter/add/3/4 => 7)
+  .get('/add/:a?/:b?', withParams,
+    ({ Counter, a, b }) => Counter.get('test').add(Number(a), Number(b))
+  )
+
+  // reset the durable
+  .get('/reset', ({ Counter }) => Counter.get('test').reset())
+
+  // 404 for everything else
+  .all('*', () => missing('Are you sure about that?'))
+
+// with itty, and using ES6 module syntax (required for DO), this is all you need
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+  fetch: router.handle
+}
+
+/*
+Example Interactions:
+
+GET /counter                                => { counter: 0 }
+GET /counter/increment                      => { counter: 1 }
+GET /counter/increment                      => { counter: 2 }
+GET /counter/increment                      => { counter: 3 }
+GET /counter/reset                          => { counter: 0 }
+GET /counter/add/20/3                       => 23
+*/
